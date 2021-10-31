623de13b4ab6deecef58f6a477118768f6ef0642
kamensky-vlad <kamensky.vo@gmail.com>
2021-10-31 19:15:53 +0500

    add k8s deploy file, jenkins build script, fix dockerfile

---
+++ ci/App.groovy
@@ -0,0 +1,35 @@
+pipeline {
+
+    agent any
+
+    options {
+        disableConcurrentBuilds()
+    }
+    parameters {
+        string(name:'user',defaultValue: 'kamenskyvo', description: 'Docker hub user')
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    sh "cd /demo && mvn package"
+                }
+            }
+        }
+        stage('Docker build'){
+            steps{
+                script{
+                    sh "cd /demo && docker build -t hello-app.jar ."
+                }
+            }
+        }
+        stage('Docker publish'){
+            steps{
+                script{
+                    sh "docker tag hello-app.jar ${params.user}/hello-app && docker push ${params.user}/hello-app"
+                }
+            }
+        }
+    }
+}
